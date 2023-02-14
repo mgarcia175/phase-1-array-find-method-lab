ec257687eebdcb8a744015d764f1aa43f9044112
Martin <martingarcia804@yahoo.com>
2023-02-13 21:39:42 -0600

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+const superbowlWin = (record) => {
+    // if (record.result === "W") return record.year
+    const found = record.find(game => game.result === "W") 
+    if (found) {
+        return found.year
+    }
+}
+
+record.find(superbowlWin)
+
+// const found = array1.find(element => element > 10);
